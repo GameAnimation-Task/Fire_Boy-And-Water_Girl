5b725f6ab9dabda2482bffc0b40fd6a8bcf86ce9
Ankoder-tech <beingankit888@gmail.com>
2026-09-09 23:10:39 +0530

    feat:Add button and platform

--- collision.js
+++ collision.js
@@ -1,4 +1,4 @@
-class Collision {
+/*class Collision {
     static update(player, platforms, keys, GAME_WIDTH, GAME_HEIGHT){
   const previousX = player.x;
 
@@ -70,6 +70,132 @@ class Collision {
     player.velocityY = 0;
     player.onGround = true;
   }
-} 
+}
+
+static checkButtonCollision(player, button) {
+
+        const collision =
+            player.x < button.x + button.width &&
+            player.x + player.width > button.x &&
+            player.y < button.y + button.height &&
+            player.y + player.height > button.y;
+
+        return collision;
+    }
+
+ static checkDiamondCollision(player, diamonds) {
+
+        for (let i = diamonds.length - 1; i >= 0; i--) {
+            const diamond = diamonds[i];
+            const collision =player.x < diamond.x + diamond.width &&player.x + player.width > diamond.x &&player.y < diamond.y + diamond.height &&
+                player.y + player.height > diamond.y;
+            if (collision) { diamonds.splice(i, 1);}
+        }
+    }
+}
+export default Collision;*/
+class Collision {
+  static update(player, platforms, keys, GAME_WIDTH, GAME_HEIGHT, dt = 16.67) {
+    const previousX = player.x;
+    let moving = false;
+
+    if (keys["ArrowLeft"]) {
+      player.x -= player.speed;
+      player.facing = -1;
+      moving = true;
+    }
+
+    if (keys["ArrowRight"]) {
+      player.x += player.speed;
+      player.facing = 1;
+      moving = true;
+    }
+
+    for (let platform of platforms) {
+      const playerTop = player.y;
+      const playerBottom = player.y + player.height;
+      const playerLeft = player.x;
+      const playerRight = player.x + player.width;
+      const verticalOverlap =
+        playerBottom > platform.y && playerTop < platform.y + platform.height;
+      const horizontalOverlap =
+        playerRight > platform.x && playerLeft < platform.x + platform.width;
+
+      if (verticalOverlap && horizontalOverlap) {
+        if (player.x > previousX) {
+          player.x = platform.x - player.width;
+        } else if (player.x < previousX) {
+          player.x = platform.x + platform.width;
+        }
+      }
+    }
+
+    const previousY = player.y;
+    player.velocityY += player.gravity;
+    player.y += player.velocityY;
+    player.onGround = false;
+
+    for (let platform of platforms) {
+      const playerLeft = player.x;
+      const playerRight = player.x + player.width;
+      const playerTop = player.y;
+      const playerBottom = player.y + player.height;
+      const previousTop = previousY;
+      const previousBottom = previousY + player.height;
+      const horizontalOverlap =
+        playerRight > platform.x && playerLeft < platform.x + platform.width;
+
+      if (
+        horizontalOverlap &&
+        player.velocityY >= 0 &&
+        previousBottom <= platform.y &&
+        playerBottom >= platform.y
+      ) {
+        player.y = platform.y - player.height;
+        player.velocityY = 0;
+        player.onGround = true;
+      }
+      if (
+        horizontalOverlap &&
+        player.velocityY < 0 &&
+        previousTop >= platform.y + platform.height &&
+        playerTop <= platform.y + platform.height
+      ) {
+        player.y = platform.y + platform.height;
+        player.velocityY = 0;
+      }
+    }
+
+    if (keys["ArrowUp"] && player.onGround) {
+      player.velocityY = player.jumpPower;
+      player.onGround = false;
+    }
+
+    if (player.x < 0) {
+      player.x = 0;
+    }
+
+    if (player.x + player.width > GAME_WIDTH) {
+      player.x = GAME_WIDTH - player.width;
+    }
+    if (player.y + player.height > GAME_HEIGHT) {
+      player.y = GAME_HEIGHT - player.height;
+      player.velocityY = 0;
+      player.onGround = true;
+    }
+
+    player.animate(moving, dt);
+  }
+  static checkButtonCollision(player, button) {
+
+    return(
+        player.x < button.x + button.width 
+        &&player.x + player.width > button.x &&
+        player.y < button.y + button.height
+        &&player.y + player.height > button.y
+    );
+    
+}
+   
 }
 export default Collision;
---
+++ platform.js
@@ -0,0 +1,12 @@
+export default class Platform {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+  draw(ctx) {
+    ctx.fillStyle = "#106436";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
